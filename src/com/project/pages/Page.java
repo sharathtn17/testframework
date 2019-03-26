1aa835240535a6f856e83b55638ccd3fa09c1c64
Sharath <sharath.n@searshc.com>
2019-03-26 14:25:17 +0530

    this commit message:q

---
+++ src/com/project/pages/Page.java
@@ -0,0 +1,12 @@
+package com.project.pages;
+
+import com.project.functionalComponet.Actions;
+
+public class Page extends Actions{
+
+	public static String getTitle()
+	{
+		String title=Actions.getTitle();
+		return title;
+	}
+}
