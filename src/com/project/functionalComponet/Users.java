2c018b59e801ec215f0e45963a357b52bbfd943b
Sharath <sharath.n@searshc.com>
2019-03-29 14:43:08 +0530

    new commit

---
+++ src/com/project/functionalComponet/Users.java
@@ -0,0 +1,15 @@
+package com.project.functionalComponet;
+
+import com.project.pages.Page;
+
+public class Users extends Page
+{
+	
+	
+	 public static final Users guestUser = new Users();
+	 
+	 
+	 
+	 
+
+}
