af9713c609f91dea7fc5c2d2e152ea0564d1686c
Sharath <sharath.n@searshc.com>
2019-05-24 13:46:12 +0530

    a

--- src/com/project/tests/TC_LoginTC.java
+++ src/com/project/tests/TC_LoginTC.java
@@ -1,9 +1,13 @@
 package com.project.tests;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.pagefactory.DefaultElementLocatorFactory;
 
 import com.project.functionalComponet.BaseTest;
@@ -15,7 +19,6 @@ import PageAction.LoginPageAction;
 public class TC_LoginTC extends BaseTest{
 	
 	
-
 	@Test
 	public void loginTest() throws IOException 
 	{
@@ -26,18 +29,25 @@ public class TC_LoginTC extends BaseTest{
 		LoginPage lp=new LoginPage(driver);
 		lp.typetext("isdisplayed in selenium")
 		.submitb()
-		
+		sys
 		.VerifyLogin();*/
+	//	System.out.println(username +" "+ pwd +" "+env+" "+inti);
+		Users.driver.get("https://freecrm.com/");
+		Actions act=new Actions(driver);
+		WebElement CRM=driver.findElement(By.xpath("//a[contains(text(),'CRM')]"));
 		
-		Users.guestUser
+		act.moveToElement(CRM).build().perform();
+		
+		driver.findElement(By.xpath("//a[contains(text(),'Call Center')]")).click();
+		System.out.println("Hi");
 		//._homePage()
-		.Loginpage()
+		/*.Loginpage()
 		.setUserName("wqqwqwq")
 		//.setPassword("qwqwqwq")
 		.clickSubmit()
 		.VerifyLogin()
 		._loginPage()
-		.setPassword("dsdsdd");
+		.setPassword("dsdsdd");*/
 		
 		
 		
@@ -48,7 +58,7 @@ public class TC_LoginTC extends BaseTest{
 	public Object[][] dataProvider()
 	{
 		return new Object[][]{
-			{"test"},{"test2"}
+			{"name1","pwd1","qa",10} ,{"name2","pwd2","PROD",76565765}
 		
 		                     
 	};
